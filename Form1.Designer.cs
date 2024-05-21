eab6a0ee3214eb9307703a33c88bf9cd1d1bc93f
Alessandro Lion <alessandro.lion@outlook.com>
2024-05-21 15:20:41 +0200

    Versione Iniziale

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace ALSimplePDFMerger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxFolder = new System.Windows.Forms.TextBox();
+            this.RadioButtonF = new System.Windows.Forms.RadioButton();
+            this.radioButtonL = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(61, 61);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(726, 197);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBoxFolder);
+            this.groupBox1.Controls.Add(this.RadioButtonF);
+            this.groupBox1.Controls.Add(this.radioButtonL);
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Location = new System.Drawing.Point(1, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(787, 284);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "INPUT";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(585, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(40, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxFolder
+            // 
+            this.textBoxFolder.Location = new System.Drawing.Point(74, 19);
+            this.textBoxFolder.Name = "textBoxFolder";
+            this.textBoxFolder.Size = new System.Drawing.Size(505, 20);
+            this.textBoxFolder.TabIndex = 3;
+            // 
+            // RadioButtonF
+            // 
+            this.RadioButtonF.AutoSize = true;
+            this.RadioButtonF.Checked = true;
+            this.RadioButtonF.Location = new System.Drawing.Point(11, 17);
+            this.RadioButtonF.Name = "RadioButtonF";
+            this.RadioButtonF.Size = new System.Drawing.Size(57, 17);
+            this.RadioButtonF.TabIndex = 2;
+            this.RadioButtonF.TabStop = true;
+            this.RadioButtonF.Text = "Folder:";
+            this.RadioButtonF.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonL
+            // 
+            this.radioButtonL.AutoSize = true;
+            this.radioButtonL.Enabled = false;
+            this.radioButtonL.Location = new System.Drawing.Point(11, 62);
+            this.radioButtonL.Name = "radioButtonL";
+            this.radioButtonL.Size = new System.Drawing.Size(44, 17);
+            this.radioButtonL.TabIndex = 1;
+            this.radioButtonL.Text = "List:";
+            this.radioButtonL.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 317);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "OUTPUT";
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(75, 314);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(505, 20);
+            this.textBoxOutput.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(354, 347);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 34);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Merge";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 393);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "AL PDF Simple Merger @alexpilion - O.S.S.";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxFolder;
+        private System.Windows.Forms.RadioButton RadioButtonF;
+        private System.Windows.Forms.RadioButton radioButtonL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ALSimplePDFMerger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Metodo PDF Merge PER INCLUDERE ALLEGATI PDF STATICI
+        public static void MergePDF(List<string> InFiles, string OutFile)
+        {
+            Document document = new Document();
+            //create newFileStream object which will be disposed at the end
+            using (FileStream newFileStream = new FileStream(OutFile, FileMode.Create))
+            {
+                PdfCopy writer = new PdfCopy(document, newFileStream);
+                document.Open();
+
+                foreach (string fileName in InFiles)
+                {
+                    PdfReader reader = new PdfReader(fileName);
+                    reader.ConsolidateNamedDestinations();
+
+                    for (int i = 1; i <= reader.NumberOfPages; i++)
+                    {
+                        PdfImportedPage page = writer.GetImportedPage(reader, i);
+                        writer.AddPage(page);
+                    }
+
+                    //QUESTO CODICE IN TEORIA CONSENTE DI ACCODARE ALLEGATI CON FORM DI INSERIMENTO DATI PDF 
+                    //*** NON TESTATO POTREBBE NON FUNZIONARE AFFATTO
+                    //PRAcroForm form = reader.AcroForm;
+                    //if (form != null)
+                    //{
+                    //    writer.CopyAcroForm(reader);
+                    //}
+
+                    reader.Close();
+                }
+                document.Close();
+                writer.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            FolderBrowserDialog folderBrowserDialog1;
+            string folderName;
+
+            // Show the FolderBrowserDialog.
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                folderName = folderBrowserDialog1.SelectedPath;
+                textBoxFolder.Text = folderName;
+                textBoxOutput.Text = folderName + "\\merged.pdf";
+            }
+
+            if (!RadioButtonF.Checked)
+            {
+                RadioButtonF.Checked = true;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool isOK = false;
+            try
+            {
+
+                if (RadioButtonF.Checked)
+                {
+                    var files = Directory.EnumerateFiles(textBoxFolder.Text, "*.pdf").OrderBy(filename => filename);
+                    MergePDF(files.ToList(), textBoxOutput.Text);
+                }
+                else
+                {
+                    //TODO: Merge from a copy and paste list within the richtextbox
+                }
+
+                isOK = true;
+            }
+            catch (Exception ex)
+            {
+                //MessageBox("Errore", ex.Message);
+                var result = MessageBox.Show(ex.Message, "Errore",
+                             MessageBoxButtons.OK,
+                             MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (isOK)
+                {
+                    var result2 = MessageBox.Show("File " + textBoxOutput.Text + " saved. ", "Success",
+                             MessageBoxButtons.OK,
+                             MessageBoxIcon.Information);
+                }
+            }
+
+        }
+    }
+}
